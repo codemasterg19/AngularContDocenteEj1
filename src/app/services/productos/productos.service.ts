0beb7ac38432d7806eb1f20b6985561e4579882e
codemasterg19 <pablo_jmz1@hotmail.com>
2024-08-29 21:24:06 -0500

    servicio producto

--- src/app/services/productos/productos.service.ts
+++ src/app/services/productos/productos.service.ts
@@ -2,10 +2,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+export interface Productos {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  imagen: string;
+  stock: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  private url = 'public/json/productoData.json';
+  constructor(private http: HttpClient) { }
+
+  getProductos(): Observable<Productos[]> {
+    return this.http.get<Productos[]>(this.url);
+  }
+
+  createProductos(productos: Productos): Observable<Productos> {
+  const httpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"});
+    return this.http.post<Productos>(this.url, productos, {headers: httpHeaders});
 }
+
+updateProductos(productos: Productos): Observable<Productos> {
+  const httpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"});
+    return this.http.put<Productos>(`${this.url}/${productos.id}`, productos, {headers: httpHeaders});
+}
+
+deleteProductos(productos: Productos): Observable<Productos> {
+  const httpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"});
+    return this.http.delete<Productos>(`${this.url}/${productos.id}`);
+}
+}
+  
