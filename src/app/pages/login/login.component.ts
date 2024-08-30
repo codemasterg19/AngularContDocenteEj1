42c8ae70186993fc5325a461968ba127ed39e76d
Jordy4200 <jordyvelasco3322@gmail.com>
2024-08-29 22:00:18 -0600

    si

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,50 +1,49 @@
 import { Component } from '@angular/core';
 import { UsersService } from '../../services/users/users.service';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-
-
 export class LoginComponent {
 
   form: FormGroup;
 
-  constructor(private usersService : UsersService,private formBuilder: FormBuilder){
+  constructor(private usersService: UsersService, private formBuilder: FormBuilder, private router: Router) {
     this.form = this.formBuilder.group({
       email: ["", Validators.required],
       password: ["", Validators.required],
-    })
-
-
+    });
   }
 
-  onClickRegister ():void{
+  onClickRegister(): void {
     this.usersService.register(this.form.value)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(error => console.log(error));
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
   }
 
-  onClickLogin (): void{
+  onClickLogin(): void {
     this.usersService.login(this.form.value)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(error => console.log(error));
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/home']); // Redirigir al home después del login
+      })
+      .catch(error => console.log(error));
   }
 
-  onClickLoginWithGoogle():void{
+  onClickLoginWithGoogle(): void {
     this.usersService.loginWithGoogle()
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(error => console.log(error));
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/home']); // Redirigir al home después del login con Google
+      })
+      .catch(error => console.log(error));
   }
 }
