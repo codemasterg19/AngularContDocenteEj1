4f692f646876b7f2c935d683c4108d739f8b7dca
Jordy4200 <jordyvelasco3322@gmail.com>
2024-08-29 20:55:38 -0600

    si

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { ListaproductoComponent } from './pages/listaproducto/listaproducto.comp
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { InfoproductoComponent } from './pages/infoproducto/infoproducto.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
 
     {path: 'home', component: HomeComponent},
     {path: 'listaproducto', component: ListaproductoComponent},
     {path: 'listaproducto/:id', component: InfoproductoComponent},
+    { path: 'login', component: LoginComponent },
     {path: 'acercade', component: AcercadeComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', component: Error404Component}
