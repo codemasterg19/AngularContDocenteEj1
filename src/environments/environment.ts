99c384ec64c6fb63223b6387b9ecd8f8b038d54f
codemasterg19 <pablo_jmz1@hotmail.com>
2024-09-12 08:37:54 -0500

    secreto test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    clientId: '',
-    secret: '',
+    clientId: 'AUF87Z_i9X1MEEBxcQIbG25qxLeiKLenQ15FoUK1lR46_ivmnsCm5AJ1avzBUTuRU26mxMA1pNqykSN5',
+    secret: 'EKXMWDM5sR-8FeszX_onoYJJ1PTYQif90f5UgdCphphsdUM4HcJERozAEF0hsF7F_bHAn0UKh_in-Nvw',
     apiUrl: '',
     apiKey: '',
 
