ddb92ecbeb2b543007b4977f95aad683d69c2c06
Jordy4200 <jordyvelasco3322@gmail.com>
2024-09-11 22:53:49 -0600

    shi

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    clientId: 'AUF87Z_i9X1MEEBxcQIbG25qxLeiKLenQ15FoUK1lR46_ivmnsCm5AJ1avzBUTuRU26mxMA1pNqykSN5',
+    clientId: '',
     secret: '',
 
 };
